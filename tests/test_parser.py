0f8499abe31882a18a483ab1feb93601e9c9e0a7
Robin De Schepper <robin.deschepper93@gmail.com>
2024-03-28 22:17:01 +0100

    fix tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,15 +1,15 @@
 import unittest
 
-from bsb import config
+from bsb import get_configuration_parser
 
 
 class TestYAML(unittest.TestCase):
     def test_yaml_parse(self):
-        yaml = config.get_parser("yaml")
+        yaml = get_configuration_parser("yaml")
         tree, meta = yaml.parse("some_key: 5")
         self.assertEqual({"some_key": 5}, tree)
 
     def test_yaml_generate(self):
-        yaml = config.get_parser("yaml")
+        yaml = get_configuration_parser("yaml")
         content = yaml.generate({"some_key": 5})
         self.assertEqual("some_key: 5\n", content)
